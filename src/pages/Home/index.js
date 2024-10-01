a72c62887625405ea1c564ee340fadcddb974aaf
continue <1545620489@qq.com>
2024-10-01 10:46:45 +0800

    完成每日计划静态

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
+import './index.css'
 import {
     DesktopOutlined,
     SmileOutlined,
     ContainerOutlined,
+    MenuFoldOutlined,
 
 } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+
+import { Breadcrumb, Layout, Menu, theme, Input, Button, Checkbox } from 'antd';
 import { useNavigate } from 'react-router-dom';
+// 输入框
+const { Search } = Input;
+const onSearch = (value, _e, info) => console.log(info?.source, value);
 const { Header, Content, Footer, Sider } = Layout;
 function getItem(label, key, icon, children) {
     return {
@@ -73,7 +79,7 @@ const Home = () => {
                         <Breadcrumb.Item>My-Daily-Life</Breadcrumb.Item>
                     </Breadcrumb>
                     {/* 内容书写 */}
-                    <div
+                    <div class='content'
                         style={{
                             padding: 24,
                             minHeight: 360,
@@ -81,6 +87,41 @@ const Home = () => {
                             borderRadius: borderRadiusLG,
                         }}
                     >
+                        <div class='plan'>
+                            <div className='head'>
+                                <div className='num'>
+                                    <span>0</span>
+                                    <p>提醒</p>
+                                </div>
+                                <MenuFoldOutlined style={{ fontSize: '20px', color: '#FF9500' }} />
+                            </div>
+
+                            <div className='search'>
+                                <Search
+                                    placeholder="添加每日计划"
+                                    allowClear
+                                    size="large"
+                                    onSearch={onSearch}
+                                    style={{
+                                        width: 270,
+                                    }}
+                                    // 改变按钮的颜色
+                                    enterButton={<Button type="primary" style={{ backgroundColor: '#FF9500', borderColor: '#FF9500' }}>Add</Button>}
+
+                                />
+
+                            </div>
+                            <div className='Daily-plan'>
+                                <div className='plans'>
+                                    <Checkbox >
+                                        学习
+                                    </Checkbox>
+                                    <p>del</p>
+                                </div>
+
+                            </div>
+                        </div>
+
                     </div>
                 </Content>
                 <Footer
